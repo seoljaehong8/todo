d4001c95c6ddc3d04d3e1c339d036329a61f4d62
jay <seoljaehong8@gmail.com>
2021-09-26 23:32:13 +0900

    modify rds

--- server/mypjt/settings.py
+++ server/mypjt/settings.py
@@ -93,10 +93,22 @@ WSGI_APPLICATION = 'mypjt.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME':'todos',
+        'USER':'admin',
+        'PASSWORD':'woghd2816',
+        'HOST':'database-1.cxgaxmvbweba.us-east-1.rds.amazonaws.com',
+        'PORT':'3306',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
+         },
     }
+
 }
 
 
