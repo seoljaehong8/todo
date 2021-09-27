356c94c5d40fd2d150f84549a61aa81c2171a8fc
jaehong <seol940601@gmail.com>
2021-09-27 09:17:57 +0900

    update settings

--- server/mypjt/settings.py
+++ server/mypjt/settings.py
@@ -100,9 +100,10 @@ DATABASES = {
     'default': {
         'ENGINE' : 'django.db.backends.mysql',
         'NAME':'todos',
-        'USER':'admin',
+        'USER':'jaehong',
         'PASSWORD':'woghd2816',
-        'HOST':'database-1.cxgaxmvbweba.us-east-1.rds.amazonaws.com',
+        # 'HOST':'database-1.cxgaxmvbweba.us-east-1.rds.amazonaws.com',
+        'HOST':'10.0.0.185',
         'PORT':'3306',
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
